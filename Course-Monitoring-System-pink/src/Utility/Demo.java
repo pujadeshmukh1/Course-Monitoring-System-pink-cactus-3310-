aa71a4ff6619e03b240df26b397d59a30481edf0
puja <deshmukhpooja976@gmail.com>
2022-12-27 12:36:14 +0530

    Demo class added

---
+++ Course-Monitoring-System-pink/src/Utility/Demo.java
@@ -0,0 +1,15 @@
+package Utility;
+
+import java.sql.Connection;
+
+public class Demo {
+
+public static void main(String[] args) {
+		
+		Connection conn = DBUtil.provideConnection();
+		
+		System.out.println(conn);
+		
+	}
+	
+}
